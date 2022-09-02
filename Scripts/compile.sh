5c964b81ee1408f1d513437749139d6dd2404808
Marek Mauder <marekmauder@gmail.com>
2022-09-02 16:18:47 +0200

    Updated helper scripts
    Added cleaner script, updated FPC compile script, removed cross-compile script for ARM targets - just use regular compile script.

--- Scripts/compile.sh
+++ Scripts/compile.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+set -eu
 
-pushd ..
-mkdir -p Dcu
+ROOTDIR="$(dirname "$0")/.."
+mkdir -p "$ROOTDIR/Dcu"
 
-fpc -FuImaging -FuImaging/ZLib -FuImaging/JpegLib -FuImaging/LibTiff -FiImaging -FUDcu -O3 -B -Xs -XX -Mdelphi -FEBin deskew.lpr
+OUTPUT="-FE$ROOTDIR/Bin -FU$ROOTDIR/Dcu"
+IMGDIR="$ROOTDIR/Imaging"
+UNITS="-Fu$IMGDIR -Fu$IMGDIR/ZLib -Fu$IMGDIR/JpegLib -Fu$IMGDIR/LibTiff"
+# This is how you suppress -vn set in fpc.cfg
+OPTIONS="-B -O3 -Xs -Mdelphi -vn-"
+INCLUDE="-Fi$IMGDIR"
+LIBS="-Fl$IMGDIR/LibTiff/Compiled"  
 
-popd
+fpc $OPTIONS $OUTPUT $UNITS $INCLUDE $LIBS deskew.lpr
--- Scripts/get_gui_versions.sh
+++ Scripts/get_gui_versions.sh
@@ -1,7 +1,9 @@
-set -e
+#!/bin/bash
+set -eu
 
 # get version from Lazarus project file
 MAJOR_VER=$(grep 'MajorVersionNr' ../Gui/deskewgui.lpi | grep -oE '[0-9]+' || true)
 MAJOR_VER=${MAJOR_VER:-0} # if major=0 it's not included in project file
 MINOR_VER=$(grep 'MinorVersionNr' ../Gui/deskewgui.lpi | grep -oE '[0-9]+')
 
+echo "GUI version detected: $MAJOR_VER.$MINOR_VER"
