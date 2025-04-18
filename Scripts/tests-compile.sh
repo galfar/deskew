0c26f5f9ad64dda3175c6d14a65db094cec20633
Marek Mauder <marekmauder@gmail.com>
2025-04-18 23:45:32 +0200

    Improved Linux FPC compile scripts
    Now works when executed from any directory.

--- Scripts/tests-compile.sh
+++ Scripts/tests-compile.sh
@@ -2,20 +2,17 @@
 set -eu
 
 ROOTDIR="$(dirname "$0")/.."
-mkdir -p "$ROOTDIR/Dcu"
+pushd $ROOTDIR
 
-echo $ROOTDIR
+mkdir -p "./Dcu"
 
-OUTPUT="-FE$ROOTDIR/Bin -FU$ROOTDIR/Dcu"
-IMGDIR="$ROOTDIR/Imaging"
+OUTPUT="-FE./Bin -FU./Dcu"
+IMGDIR="./Imaging"
 UNITS="-Fu$IMGDIR -Fu$IMGDIR/ZLib -Fu$IMGDIR/LibTiff"
 OPTIONS="-B -CirotR -O1 -Mdelphi -vn-h-"
 INCLUDE="-Fi$IMGDIR"
 LIBS="-Fl$IMGDIR/LibTiff/Compiled"
 DEFINES="-dPASJPEG"
 
-pushd ../Tests
+fpc $OPTIONS $OUTPUT $UNITS $INCLUDE $LIBS $DEFINES Tests/tests.lpr
 
-fpc $OPTIONS $OUTPUT $UNITS $INCLUDE $LIBS $DEFINES tests.lpr
-
-popd
